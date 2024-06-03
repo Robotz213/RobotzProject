148dfcd6bd296af9d7f9c8dcf91010fef1119960
Robotz213 <nicholasazs213@gmail.com>
2024-06-02 22:48:42 -0300

    Refact: login e dashboard compartilham arquivos, coloquei eles todos na pasta "static" dentro de App pra evitar arquivos duplicados dentro do projeto

---
+++ app/auth/__init__.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+import pathlib
+
+from clear import clear
+
+this_path = pathlib.Path(__file__).parent.resolve().__str__()
+
+dashboard_blueprint = Blueprint('dashboard', __name__, template_folder = 'templates_dashboard', static_folder = this_path)
+
+@dashboard_blueprint.route("/")
+def dashboard():
+    
+    pagina = "includes/dashboard.html"
+    return render_template("index.html", pagina = pagina)
+
