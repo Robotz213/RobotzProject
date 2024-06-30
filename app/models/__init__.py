3bd926498e06e6832d6df8f3fb4bf52f7bd8ac20
Robotz213 <nicholasazs213@gmail.com>
2024-06-30 01:31:52 -0400

    att

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app import app
 from app import db
+import openpyxl
 from datetime import datetime
 from app.models.sql.sqlite3.auth import Users
 from app.models.sql.mysql.law.processos import Processos
@@ -21,29 +22,37 @@ with app.app_context():
                     license_key = root_license)
         
         db.session.add(usuario)
-        
-    proc = Processos.query.filter(Processos.processo == "0600305-93.2022.8.04.6300").first()
     
-    if proc is None:
+    ws = openpyxl.load_workbook(r"R:\Github\RobotzProject\Sucessos 30-06-24.xlsx").active
+    
+    for i in range(2, ws.max_row):
         
-        date_string = '28-01-2022'
         format_string = '%d-%m-%Y'
-
-        try:
-            date_obj = datetime.strptime(date_string, format_string)
-            print(date_obj)  # This will print: 2022-01-28 00:00:00
-        except ValueError as e:
-            print("Error:", e)
+        numeroproc = ws.cell(row=i, column=1).value
+        autor = ws.cell(row=i, column=2).value
+        reu = ws.cell(row=i, column=3).value
+        status = ws.cell(row=i, column=4).value
+        distribuicao_data = ws.cell(row=i, column=5).value.replace("/", "-")
+        distribuicao_data = datetime.strptime(distribuicao_data, format_string)
+        valor_causa = ws.cell(row=i, column=6).value
+        proc = Processos.query.filter(Processos.processo == numeroproc).first()
         
-        processo = Processos(
-            processo = "0600305-93.2022.8.04.6300",
-            autor = "PORFIRIA ANEZIA CONCEICAO BARB",
-            reu = "AMAZONAS DISTRIBUIDORA DE ENERGIA S.A",
-            status = "ARQUIVADO",
-            distribuicao_data = date_obj,
-            valor_causa = "21000.00"  
-        )
+        if proc is None:
+            
+            processo = Processos(
+                processo = numeroproc,
+                autor = autor,
+                reu = reu,
+                status = status,
+                distribuicao_data = distribuicao_data,
+                valor_causa = valor_causa  
+            )
+            
+            db.session.add(processo)
         
-        db.session.add(processo)
+        if i == ws.max_row:
+            break
+    
+    
         
     db.session.commit()
